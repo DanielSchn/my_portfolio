52ccc2b7b8522d7b428ba019ef4983ac26e2701c
DanielSchn <d.schneider@dsc-mail.de>
2024-03-12 11:33:54 +0100

    skills design

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
 
+  skillsList = [
+    {
+      name: 'JavaScript',
+      url: './assets/img/icons/javascript.png'
+    },
+    {
+      name: 'Angular',
+      url: './assets/img/icons/angular.png'
+    },
+    {
+      name: 'TypeScript',
+      url: './assets/img/icons/typescript.png'
+    },
+    {
+      name: 'HTML5',
+      url: './assets/img/icons/html.png'
+    },
+    {
+      name: 'Git',
+      url: './assets/img/icons/git.png'
+    },
+    {
+      name: 'Firebase',
+      url: './assets/img/icons/firebase.png'
+    },
+    {
+      name: 'CSS',
+      url: './assets/img/icons/css.png'
+    },
+    {
+      name: 'Scrum',
+      url: './assets/img/icons/scrum.png'
+    },
+    {
+      name: 'Rest Api',
+      url: './assets/img/icons/rest_api.png'
+    },
+    {
+      name: 'Material<br>Design',
+      url: './assets/img/icons/materialdesign.png'
+    }
+  ];
 }
