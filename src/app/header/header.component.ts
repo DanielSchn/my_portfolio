c4c7ac3a60ef7228278ca2538864086690a25500
DanielSchn <d.schneider@dsc-mail.de>
2024-03-18 13:16:29 +0100

    responsive + burger menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  navbarHidden =  true;
 
+
+  ngOnInit() {
+    document.body.addEventListener('click', this.closeNavbar.bind(this));
+  }
+
+
+  ngOnDestroy() {
+    document.body.removeEventListener('click', this.closeNavbar.bind(this));
+  }
+
+  
+  toggleNavbar(event: MouseEvent) {
+    event.stopPropagation();
+    this.navbarHidden = !this.navbarHidden;
+  }
+
+
+  closeNavbar() {
+    this.navbarHidden = true;
+  }
 }
