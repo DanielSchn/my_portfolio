bbd4c61b128b7e7a50da9db8ad90215ef3ac225c
DanielSchn <d.schneider@dsc-mail.de>
2024-03-22 12:53:15 +0100

    refactoring

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -23,7 +23,7 @@ export class ContactComponent {
   };
 
   mailTest = true;
-  showSubmitMessage:any = false;
+  showSubmitMessage: any = false;
 
   post = {
     endPoint: 'https://dschneider-dev.de/sendMail.php',
@@ -49,12 +49,15 @@ export class ContactComponent {
           complete: () => console.info('send post complete'),
         });
     } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) { // Am ende muss dieser else if teil weg oder oben mailTest auf false
-      console.log('sending successfull');
       ngForm.resetForm();
-      this.showSubmitMessage = true;
-      setTimeout(() => {
-        this.showSubmitMessage = false;
-      }, 3000);
+      this.showMessage();
     }
+  }
+
+  showMessage() {
+    this.showSubmitMessage = true;
+    setTimeout(() => {
+      this.showSubmitMessage = false;
+    }, 3000);
   }
 }
