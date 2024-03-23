7153d7c9c0c2ef44ecabc5f26153f2a796fd472c
DanielSchn <d.schneider@dsc-mail.de>
2024-03-23 16:29:55 +0100

    gsap

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 
 @Component({
@@ -11,7 +12,30 @@ import { gsap } from 'gsap';
   templateUrl: './aboutme.component.html',
   styleUrl: './aboutme.component.scss',
 })
-export class AboutmeComponent {
-  constructor(public translate: TranslateService) { }
 
+export class AboutmeComponent {
+  @ViewChild('myH1') myH1!: ElementRef;
+
+  constructor(public translate: TranslateService) {
+    gsap.registerPlugin(ScrollTrigger);
+  }
+
+  ngAfterViewInit() {
+    this.initScrollAnimation();
+  }
+
+  initScrollAnimation() {
+    const h1Element = this.myH1.nativeElement;
+
+    gsap.from(h1Element, {
+      x: -100, // Startposition links vom Bildschirm
+      opacity: 0,
+      duration: 1.5, // Dauer der Animation in Sekunden
+      scrollTrigger: {
+        trigger: h1Element,
+        start: "top 80%", // Startet die Animation, wenn "top" von h1 "bottom" des Viewports erreicht
+        toggleActions: "play none none none"
+      }
+    });
+  }
 }
